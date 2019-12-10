4d698fadac9c614686b8ebb331b03d48af964cf1
Madeleine Ammar <madeleine.ammar@gmail.com>
2019-12-10 10:13:06 +0100

    added another line including the link to the gapminder-file download

--- analysis.R
+++ analysis.R
@@ -2,4 +2,7 @@
 
 #first lets download the gapminder data
 
-#V
+download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder_data.csv", destfile = "data/gapminder_data.csv")
+
+gapminder <- read.csv("data/gapminder_data.csv")
+
