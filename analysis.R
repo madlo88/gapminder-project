ec5be5adcb20be1243af8251920883c35d1b51e7
Madeleine Ammar <madeleine.ammar@gmail.com>
2019-12-10 10:54:27 +0100

    added a word to anaylsis.R

--- analysis.R
+++ analysis.R
@@ -2,7 +2,5 @@
 
 #first lets download the gapminder data
 
-download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder_data.csv", destfile = "data/gapminder_data.csv")
-
-gapminder <- read.csv("data/gapminder_data.csv")
+#new word
 
